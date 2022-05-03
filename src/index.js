412c998637eefa8e6253af06689f0ac84224ba43
Hoang Nguyen <hoangnguyen21091996@gmail.com>
2022-05-03 13:36:00 +0700

    remove

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 3000;
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(morgan('dev'));
+
+app.engine('hbs', engine({
+  extname: '.hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources\\views'));
+
+
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
